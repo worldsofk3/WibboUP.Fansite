e5b87a9bed598bed9949edf26503dfbe0d79f1eb
worldsofk3 <qdmk3ntin@gmail.com>
2023-11-25 21:32:40 +0100

    Index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,68 +1,59 @@
 <template>
     <Navbar />
-    <section class="grid grid-cols-2 gap-2 sm:w-2/3 mx-auto">
-        <div class=" flex flex-col justify-between">
-            <div class="col-span-1">
-                <div>
-                    <img src="https://imaging.wibbo.org/?figure=hr-170-31.hd-209-1359.lg-280-110.sh-3035-110.ch-3030-110.he-3858-110&head_direction=3&gesture=sml" />
-                    <div>
-                        <p>Python</p>
-                        <p>
-                            <img src="@/assets/imgs/mottohome.gif" />Python vous souhaite la
-                            bienvenue sur notre fansite officiel
-                        </p>
+    <section class="grid grid-cols-[600px_300px] gap-5 sm:w-2/3 mx-auto">
+        
+            <div class="col-span-1 max-w[600px]">
+               
+                <div class="mb-5 rounded-md flex items-center justify-evenly overflow-hidden bg-[url('https://images.habbo.com/c_images/catalogue/feature_cata_hort_scifi17bun1.png')] w-[600px] h-[160px]">
+                    
+                    <img class="h-[194px] w-[128px] object-cover img-pixelated border-white-collapse hover:scale-110 transition-transform" src="https://imaging.wibbo.org/?figure=ch-3368-110-1408.hd-180-2.lg-3596-110-1408.he-3082-63.sh-295-110.ca-3177-110-73.cc-3827-1263-1230.wa-5238-1330.ea-1402-73.fa-1201-1408.hr-3871-61&head_direction=3&gesture=sml" />
+                    
+                    <div class="text-white bg-[#22222280] w-[374px] h-[148px] p-3 rounded-md">
+                        <p class="text-3xl">Jason</p>
+                        <div class="flex items-center">
+                            <img class="w-[15px] h-[15px] mr-1" src="@/assets/imgs/mottohome.gif" />
+                            <p class="overflow-auto">Développeur de WibboUp !</p>
+                        </div>
                     </div>
                 </div>
+            
 
-                    <div>
-                        <div>
-                            <h4>Version bêta</h4>
-                            <p>
+                        <div class="bg-[url('@/assets/imgs/index/news.png')] min-h-[250px] mb-5 rounded-md flex flex-col justify-evenly p-10">
+                            <h4 class="text-xl font-bold text-white">Version bêta</h4>
+                            <p class="text-white max-w-[300px]">
                                 Notre version bêta débarque et reste temporaire le temps du
                                 développement.
                             </p>
-                            <a href="#"><button type="button">Lire la suite »</button></a>
+                            <a href="#"><button type="button" class="text-[#3c3c3c] bg-white p-[5px] border rounded-md text-[12px]">Lire la suite »</button></a>
                         </div>
 
-                        <div>
-                            <h4>Bienvenue sur Wibbo'Up !</h4>
-                            <p>Et oui, enfin..</p>
-                            <a href="#"><button type="button">Lire la suite »</button></a>
-                        </div>
+            </div>
+            
+            <div>
+                    <div class="col-span-1 bg-[#efefef] h-min p-2 rounded-md box-shadow-type-1 border-b-2 border-[#9f9f9f] mb-5">
+                            <h4 class="bg-[#3d72ab] p-1 w-full text-center text-white rounded-md">Wibbo du mois</h4>
+                            <div class="flex items-center">
+                                <img src="https://imaging.wibbo.org/?figure=ch-3368-110-1408.hd-180-2.lg-3596-110-1408.he-3082-63.sh-295-110.ca-3177-110-73.cc-3827-1263-1230.wa-5238-1330.ea-1402-73.fa-1201-1408.hr-3871-61&direction=2&gesture=sml&size=m">
+                                    <div>
+                                        <p class="text-xs text-[#565656]">Le Wibbo du mois de Novembre 2023 est :</p>
+                                        <p class="text-3xl text-[#565656]">Jason</p>
+                                        <p class="float-right text-xs underline text-[#3d72ab]">Voir le classement</p>
+                                    </div>
+                            </div>
                     </div>
 
-                    <div>
-                        <h5>Discord</h5>
+                    <div class="col-span-1 bg-[#efefef] h-min p-2 rounded-md box-shadow-type-1 border-b-2 border-[#9f9f9f]">
+                        <h4 class="bg-[#3d72ab] p-1 w-full text-center text-white rounded-md mb-2">Discord</h4>
                         <iframe
                             src="https://discord.com/widget?id=1170468974992031856&theme=dark"
-                            width="350"
+                           width="284"
                             height="200"
                             allowtransparency="true"
                             frameborder="0"
                             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
                         ></iframe>
                     </div>
-            </div>
-            
-        </div>
-        <div class="col-span-1">
-            <div class="flex justify-end">
-                <img src="@/assets/imgs/connexion.gif" />
-                    <form class="flex flex-col">
-                            <label for="username">Espace Staff</label>
-                            <input
-                                id="username"
-                                type="text"
-                                placeholder="Ton pseudo Wibbo..."
-                            />
-                            <input
-                                id="password"
-                                type="password"
-                                placeholder="Ton mot de passe..."
-                            />
-                    </form>
-            </div>
-        </div>
+                </div>
+        <Footer />
 	</section>
-    <Footer />
 </template>
