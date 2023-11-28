3f21b5bbaeb991e6ff29478b7f641006559ec003
Kodamas <superjason95@gmail.com>
2023-11-28 11:33:14 +0100

    fix: jsonwebtoken

--- src/server/utils/jsonwebtoken.ts
+++ src/server/utils/jsonwebtoken.ts
@@ -1,6 +1,6 @@
-import { sign } from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken'
 
-export function generateJWTToken (userId: number): string {
+export const generateJWTToken = (userId: number) => {
   const JWT_SECRET = '123'
   const JWT_EXPIRATION = '1h'
 
@@ -8,6 +8,6 @@ export function generateJWTToken (userId: number): string {
     throw new Error('La clé secrète JWT n\'est pas définie dans les variables d\'environnement')
   }
 
-  const token = sign({ userId }, JWT_SECRET, { expiresIn: JWT_EXPIRATION })
+  const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: JWT_EXPIRATION })
   return token
 }
